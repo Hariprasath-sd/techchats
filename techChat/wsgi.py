e7fb50e166ed461880e74cc5bed22c3682f1b14a
Hariprasath-sd <mshariprasath57@gmail.com>
2025-09-26 17:45:18 +0530

    Update wsgi.py

--- techChat/wsgi.py
+++ techChat/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cls.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'techChat.settings')
 
 application = get_wsgi_application()
